beefd4273698bf2ae40189791a51e05be5781584
Valentin Nigolian <val@schroeder.inf.unibe.ch>
2024-07-01 16:33:33 +0200

    cleaned up boundary mapping

--- src/ShrinkAndExpand.cc
+++ src/ShrinkAndExpand.cc
@@ -17,10 +17,10 @@ bool extract_mesh_name_and_directory(std::string location,
                                      std::string& directory);
 
 
-void writeResultMeshInSubdirectory(std::string location,
-                                   std::string sub_directory,
-                                   TetrahedralMesh& result_mesh,
-                                   std::string suffix = "output");
+void write_mesh(std::string location,
+                std::string sub_directory,
+                TetrahedralMesh& mesh,
+                std::string suffix = "output");
 
 
 void export_input_and_output_for_FOF_and_TLC_comparison(std::string location,
@@ -31,17 +31,20 @@ void export_input_and_output_for_FOF_and_TLC_comparison(std::string location,
 
 
 void print_usage(){
-    std::cerr<<" EXPECTED USAGE: ShrinkAndExpand [input_mesh] [output_location] [function_index] [boundary_mapping] [option]"<<std::endl;
+    std::cerr<<" EXPECTED USAGE: ShrinkAndExpand [input_mesh] [output_mesh] [function_index] [boundary_mapping] [option]"<<std::endl;
     std::cout<<"   ---------------------------------------------------- "<<std::endl;
     std::cout<<"   [input_mesh] the input .ovm mesh file"<<std::endl;
     std::cout<<"   ---------------------------------------------------- "<<std::endl;
-    std::cout<<"   [output_location] where the output .ovm mesh will go, along with a .json file containing some expansion stats"<<std::endl;
+    std::cout<<"   [output_mesh] the output .ovm mesh file. A .json file containing some expansion stats will created at the same location"<<std::endl;
     std::cout<<"   ---------------------------------------------------- "<<std::endl;
     std::cout<<"   with [function_index] in:"<<std::endl;
     std::cout<<"        0 - shrink-and-expand method "<<std::endl;
-    std::cout<<"        1 - map mesh's boundary to be star-shaped"<<std::endl;
+    std::cout<<"        1 - map mesh's boundary to the specified star-shaped boundary (defined by boundary_mapping)"<<std::endl;
     std::cout<<"   ---------------------------------------------------- "<<std::endl;
     std::cout<<"   [boundary_mapping]: "<<std::endl;
+    std::cout<<"   for function_index = 0:"<<std::endl;
+    std::cout<<"       path to a .ovm mesh with prescribed boundary positions or a text file with the per-vertex positions (see documentation)"<<std::endl;
+    std::cout<<"   for function_index = 1:"<<std::endl;
     std::cout<<"        1 - tetrahedral boundary"<<std::endl;
     std::cout<<"        2 - stiff tetrahedral boundary"<<std::endl;
     std::cout<<"        3 - tetrahedral boundary then spherical projection"<<std::endl;
@@ -56,7 +59,7 @@ void print_usage(){
     std::cout<<"            (This is NOT a debugging mode)"<<std::endl;
     std::cout<<"   for function_index = 1:"<<std::endl;
     std::cout<<"        0 -    output only export "<<std::endl;
-    std::cout<<"        1 - input + output export (including boundary cleanup)"<<std::endl;
+    //std::cout<<"        1 - input + output export (including boundary cleanup)"<<std::endl;
     std::cout<<"        2 - input + output export + TLC formatted"<<std::endl;
     std::cout<<"   ---------------------------------------------------- "<<std::endl;
 }
@@ -69,8 +72,6 @@ void print_usage(){
 **/
 int main(int argc, char** argv) {
 
-
-#warning TODO: rename executable for something like "BatchProcessor"
     if(argc < 5 || argc > 6){
         print_usage();
         return -1;
@@ -89,11 +90,13 @@ int main(int argc, char** argv) {
 
 
     ss = std::istringstream(argv[2]);
-    std::string output_path;
-    if (!(ss >> output_path)) {
+    std::string output_mesh_path;
+    if (!(ss >> output_mesh_path)) {
         std::cerr << "Invalid output path argument: " << argv[2] << '\n';
     } else if (!ss.eof()) {
         std::cerr << "Trailing characters after location argument: " << argv[2] << '\n';
+    }else{
+        std::cout<<" writing output mesh to "<<output_mesh_path<<std::endl;
     }
 
 
@@ -108,10 +111,16 @@ int main(int argc, char** argv) {
 
 
     int boundary_map(0);
+    std::string boundary_path;
     if(argc >= 5){
         ss = std::istringstream(argv[4]);
-        ss >> boundary_map;
-        std::cout<<" map: "<<boundary_map<<std::endl;
+        if(function_index) {
+            ss >> boundary_map;
+            std::cout << " map: " << boundary_map << std::endl;
+        }else{
+            ss >> boundary_path;
+            std::cout << " boundary counditions given by " << boundary_path << std::endl;
+        }
     }
 
     int option(0);
@@ -129,7 +138,7 @@ int main(int argc, char** argv) {
 
     case 0:{
 
-        std::cout<<"  SHRINKING AND EXPANDING MESH "<<input_mesh_path<<std::endl;
+        std::cout<<"  SHRINKING AND EXPANDING MESH "<<input_mesh_path<<" to boundary conditions prescribed by "<<boundary_path<<std::endl;
 
         std::string filename, directory;
         if(!extract_mesh_name_and_directory(input_mesh_path, filename, directory)){
@@ -137,29 +146,11 @@ int main(int argc, char** argv) {
                 exit(EXIT_FAILURE);
         }
         std::string boundary = "";
-        switch(boundary_map){
-        case 1:{
-            boundary = "tet_mapped";
-            break;
-        }
-        case 2:{
-            boundary = "stiff_tet_mapped";
-            break;
-        }
-        case 3:{
-            boundary = "ball_mapped";
-            break;
-        }
-        case 4:{
-            boundary = "random_star_shape_mapped";
-            break;
-        }
-        default:{
-            std::cout<<" ERROR - unhandled boundary "<<boundary_map<<std::endl;
-            print_usage();
-            return -1;
-        }
-        }
+
+        std::cout<<" input director/file: "<<directory<<" /"<<filename<<std::endl;
+
+        std::cout<<" stopping for now"<<std::endl;
+        exit(EXIT_FAILURE);
 
         int result = BatchProcessor::collapseSingleMesh(ProgressiveEmbedder::shrinkAndExpand,
                                                         std::string(input_mesh_path.c_str()),
@@ -169,7 +160,8 @@ int main(int argc, char** argv) {
                                                         input_mesh,
                                                         result_mesh,
                                                         filename,
-                                                        output_path+"/"+filename+"_"+boundary+".json");
+                                                        output_mesh_path);
+                                                        //output_path+"/"+filename+"_"+boundary+".json");
 
         switch(result){
         case 0: {
@@ -177,10 +169,10 @@ int main(int argc, char** argv) {
             std::cout<<" ==== SUCCESFULLY MAPPED MESH TO A STAR-SHAPED DOMAIN WITH STRICTLY POSITIVE-VOLUME TETS ===="<<std::endl;
             std::cout<<" ============================================================================================"<<std::endl;
 
-            writeResultMeshInSubdirectory(input_mesh_path,
-                                          output_path,
-                                          result_mesh,
-                                          "output_"+boundary);
+            write_mesh(input_mesh_path,
+                       "",//output_path,
+                       result_mesh,
+                       "");//"output_" + boundary);
             break;
         }
 
@@ -216,46 +208,45 @@ int main(int argc, char** argv) {
 
     case 1:{
 
-        std::cout<<"  MAPPING TO STAR-SHAPED BOUNDARY MESH "<<input_mesh_path<<std::endl;
+        std::cout<<"  MAPPING BOUNDARY OF MESH "<<input_mesh_path<<" TO A STAR-SHAPED CODOMAIN"<<std::endl;
 
         int result(1);
 
-        std::string output_sub_directory = "";
         switch(boundary_map){
         case 1:{
             result = BatchProcessor::collapseSingleMesh(ProgressiveEmbedder::map_to_unit_tet,
                                                         std::string(input_mesh_path.c_str()),
-                                                        true,
+                                                        false,
                                                         input_mesh,
                                                         result_mesh);
-            output_sub_directory = "tet_mapped";
+            //output_sub_directory = "tet_mapped";
             break;
         }
         case 2:{
             result = BatchProcessor::collapseSingleMesh(ProgressiveEmbedder::map_to_stiff_unit_tet,
                                                         std::string(input_mesh_path.c_str()),
-                                                        true,
+                                                        false,
                                                         input_mesh,
                                                         result_mesh);
-            output_sub_directory = "stiff_tet_mapped";
+            //output_sub_directory = "stiff_tet_mapped";
             break;
         }
         case 3:{
             result = BatchProcessor::collapseSingleMesh(ProgressiveEmbedder::map_to_unit_ball_using_tet,
                                                         std::string(input_mesh_path.c_str()),
-                                                        true,
+                                                        false,
                                                         input_mesh,
                                                         result_mesh);
-            output_sub_directory = "ball_mapped";
+            //output_sub_directory = "ball_mapped";
             break;
         }
         case 4:{
             result = BatchProcessor::collapseSingleMesh(ProgressiveEmbedder::map_to_random_star_shape_using_tet,
                                                         std::string(input_mesh_path.c_str()),
-                                                        true,
+                                                        false,
                                                         input_mesh,
                                                         result_mesh);
-            output_sub_directory = "random_star_shape_mapped";
+            //output_sub_directory = "random_star_shape_mapped";
             break;
         }
         default:{
@@ -272,7 +263,8 @@ int main(int argc, char** argv) {
             std::cout<<" ======= SUCCESFULLY MAPPED MESH BOUNDARY TO A STAR-SHAPED DOMAIN ======="<<input_mesh_path<<std::endl;
             std::cout<<" ========================================================================"<<std::endl;
 
-            std::cout<<" option = "<<option<<std::endl;
+            //std::cout<<" option = "<<option<<std::endl;
+            std::string output_sub_directory = "";
             if(option == 2){
 
 
@@ -287,16 +279,16 @@ int main(int argc, char** argv) {
                 if(option == 1){
                     std::cout<<" copying input "<<std::endl;
 
-                    writeResultMeshInSubdirectory(input_mesh_path,
-                                                  output_sub_directory,
-                                                  input_mesh,
-                                                  output_sub_directory+"_input");
+                    write_mesh(input_mesh_path,
+                               output_sub_directory,
+                               input_mesh,
+                               output_sub_directory + "_input");
                 }
 
-                writeResultMeshInSubdirectory(input_mesh_path,
-                                              output_sub_directory,
-                                              result_mesh,
-                                              output_sub_directory);
+                write_mesh(output_mesh_path,
+                           output_sub_directory,
+                           result_mesh,
+                           output_sub_directory);
             }
             break;
         }
@@ -358,24 +350,30 @@ bool extract_mesh_name_and_directory(std::string location,
 
 
 
-void writeResultMeshInSubdirectory(std::string location,
-                                   std::string sub_directory,
-                                   TetrahedralMesh& result_mesh,
-                                   std::string suffix){
+void write_mesh(std::string output_file,
+                std::string sub_directory,
+                TetrahedralMesh& mesh,
+                std::string suffix){
 
     std::cout<<"-------------------------------"<<std::endl;
-    std::cout<<" --> WRITING RESULT MESH "<<location<<std::endl;
-    std::cout<<" result mesh stats: "<<std::endl;
-    std::cout<<"  - vertices........"<<result_mesh.n_vertices()<<std::endl;
-    std::cout<<"  -    edges........"<<result_mesh.n_edges()<<std::endl;
-    std::cout<<"  -    faces........"<<result_mesh.n_faces()<<std::endl;
-    std::cout<<"  -    cells........"<<result_mesh.n_cells()<<std::endl;
+    std::cout<<" --> WRITING MESH TO "<<output_file<<std::endl;
+    std::cout<<"  mesh stats: "<<std::endl;
+    std::cout << "  - vertices........" << mesh.n_vertices() << std::endl;
+    std::cout << "  -    edges........" << mesh.n_edges() << std::endl;
+    std::cout << "  -    faces........" << mesh.n_faces() << std::endl;
+    std::cout << "  -    cells........" << mesh.n_cells() << std::endl;
 
     OpenVolumeMesh::IO::FileManager fileManager;
 
     std::string filename, directory;
+    if(!fileManager.writeFile(output_file,
+                              mesh)){
+        std::cerr<<" ERROR - COULD NOT WRITE FILE "<<directory + "/"+ sub_directory + "/" + filename + ".ovm"<<std::endl;
+    }else{
+        std::cout<<" wrote mesh as "<<output_file<<std::endl;
+    }
 
-    if (extract_mesh_name_and_directory(location, filename, directory)) {
+    /*if (extract_mesh_name_and_directory(location, filename, directory)) {
 
         std::string output_location = directory + "/" + sub_directory + "/" + filename + "_"+suffix+".ovm";
 
@@ -385,9 +383,9 @@ void writeResultMeshInSubdirectory(std::string location,
         std::cout<<" sub directory = "<<sub_directory<<std::endl;
         std::cout<<" output file: "<<output_location<<std::endl;
 
-        result_mesh.collect_garbage();
+        mesh.collect_garbage();
         if(!fileManager.writeFile(output_location,
-                                  result_mesh)){
+                                  mesh)){
             std::cerr<<" ERROR - COULD NOT WRITE FILE "<<directory + "/"+ sub_directory + "/" + filename + ".ovm"<<std::endl;
         }else{
             std::cout<<" wrote mesh as "<<output_location<<std::endl;
@@ -397,16 +395,17 @@ void writeResultMeshInSubdirectory(std::string location,
 
 
         //std::cerr<<" ERROR - parsing directory path, performing emergency save at "<<(location + "_" + sub_directory + ".ovm")<<std::endl;
-        //fileManager.readFile(location, result_mesh);
+        //fileManager.readFile(location, mesh);
         //fileManager.writeFile(location + "_" + sub_directory + ".ovm", mesh);
-    }
+    }*/
+
     std::cout<<" --> DONE"<<std::endl;
     std::cout<<"-------------------------------"<<std::endl;
 
 }
 
 
-void copyInputMeshInSubdirectory(std::string location, std::string sub_directory){
+/*void copyInputMeshInSubdirectory(std::string location, std::string sub_directory){
     std::cout<<"-------------------------------"<<std::endl;
     std::cout<<" --> COPYING MESH "<<location<<std::endl;
     OpenVolumeMesh::IO::FileManager fileManager;
@@ -430,7 +429,7 @@ void copyInputMeshInSubdirectory(std::string location, std::string sub_directory
     }
     std::cout<<" --> DONE"<<std::endl;
     std::cout<<"-------------------------------"<<std::endl;
-}
+}*/
 
 
 
